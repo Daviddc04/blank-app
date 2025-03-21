2b5ab07273b725629910e01db7b564754027d8ce
David <David.Cherry@city.ac.uk>
2025-03-21 11:40:32 +0000

    Update Stock Prediction App

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
+import pandas as pd
+from prophet import Prophet
+
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_csv('https://raw.githubusercontent.com/facebook/prophet/main/examples/example_wp_log_peyton_manning.csv')
+df.head()
+
+m = Prophet()
+m.fit(df)
+
+future = m.make_future_dataframe(periods=365)
+future.tail()
+
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+fig1 = m.plot(forecast)
+
+fig2 = m.plot_components(forecast)
